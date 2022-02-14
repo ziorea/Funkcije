f6a2ba55031beebd602fca7e3b95364ce6401e41
martina <martina.kolar7@gmail.com>
2022-02-14 15:50:42 +0100

    init

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,29 +1,37 @@
 package com.leetcode;
 
 public class Main {
+    public static boolean isDivisable(int djeljenik, int djeljitelj) {
+        if (djeljenik % djeljitelj == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void printResult(int i, int a, int b, int c) {
+        if (isDivisable(i, a)) {
+            System.out.println(i + " je djeljiv sa " + a);
+        }
+        else if (isDivisable(i, b)) {
+            System.out.println(i + " je djeljiv sa " + b);
+        }
+        else if (isDivisable(i, c)) {
+            System.out.println(i + " je djeljiv sa " + c);
+        }
+        else {
+            System.out.println(i + " nije djeljiv sa ni jednim brojem");
+        }
+    }
+
+    public static void printRange(int start, int stop, int a, int b, int c) {
+        for (int i = start; i <= stop; i++) {
+            printResult(i, a, b, c);
+        }
+    }
+
 
     public static void main(String[] args) {
-
-        for (int i = 10; i <= 20; i++) {
-
-            int a = 3;
-            int b = 7;
-            int c = 11;
-
-            boolean truea = (i % a == 0);
-            boolean trueb = (i % b == 0);
-            boolean truec = (i % c == 0);
-            boolean falseabc = (i % a != 0 && i % b != 0 && i % c != 0);
-
-            if (truea) {
-                System.out.println("Broj je djeljiv s 3.");
-            } else if (trueb) {
-                System.out.println("Broj je djeljiv sa 7.");
-            } else if (truec) {
-                System.out.println("Broj je djeljiv sa 11.");
-            } else if (falseabc) {
-                System.out.println("Broj nije djeljiv.");
-            }
-        }
+        printRange(10, 20, 3, 7, 11);
     }
 }
